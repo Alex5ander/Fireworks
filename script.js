3b3f6bd069109fe1e1d98e41cdc32f564aba49c6
Alex5ander <alexsandergutierreez@gmail.com>
2021-09-29 23:28:11 -0300

    Correção tempo decorrido

--- script.js
+++ script.js
@@ -4,24 +4,26 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var objects = [];
 var gravity = 0.98;
-function rand(min, max){
+var elapsedTime = 0;
+
+function rand(min, max) {
 	return min + Math.floor(Math.random() * ((max + 1) - min));
 }
-function randColor(){
-	return "rgb("+rand(0, 255)+", "+rand(0, 255)+", "+rand(0, 255)+")";
+function randColor() {
+	return "rgb(" + rand(0, 255) + ", " + rand(0, 255) + ", " + rand(0, 255) + ")";
 }
 
-class Particle{
-	constructor(x, y, radius, force, color){
+class Particle {
+	constructor(x, y, radius, force, color) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
 		this.color = color;
-		this.vel = {x:0, y:0};
+		this.vel = { x: 0, y: 0 };
 		this.force = force;
-		this.time = 1;
+		this.time = 5;
 	}
-	draw(){
+	draw() {
 		ctx.save();
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
@@ -31,50 +33,50 @@ class Particle{
 		ctx.fill();
 		ctx.restore();
 	}
-	update(){
+	update() {
 		this.vel.y += gravity;
-		
+
 		this.vel.x += this.force.x;
 		this.vel.y += this.force.y;
-		
-		this.x += this.vel.x;
-		this.y += this.vel.y;
-		
-		this.time -= .09;
-		
-		this.force = {x :0, y: 0};
+
+		this.x += this.vel.x * elapsedTime;
+		this.y += this.vel.y * elapsedTime;
+
+		this.time -= 1 * elapsedTime;
+
+		this.force = { x: 0, y: 0 };
 	}
 }
 
-class Firework extends Particle{
-	constructor(x, y, radius, force){
+class Firework extends Particle {
+	constructor(x, y, radius, force) {
 		super(x, y, radius, force);
 		this.color = randColor();
 	}
-	update(){
+	update() {
 		this.vel.y += gravity;
-		
+
 		this.vel.x += this.force.x;
 		this.vel.y += this.force.y;
-		
-		this.x += this.vel.x;
-		this.y += this.vel.y;
-		
-		if(this.vel.y >= 0){
+
+		this.x += this.vel.x * elapsedTime;
+		this.y += this.vel.y * elapsedTime;
+
+		if (this.vel.y >= 0) {
 			this.time = 0;
 			this.explode();
 		}
-		
-		this.force = {x:0, y:0};
+
+		this.force = { x: 0, y: 0 };
 	}
-	explode(){
-		for(var i = 0; i < 100; i++){
+	explode() {
+		for (var i = 0; i < 100; i++) {
 			objects.push(
 				new Particle(
-					this.x, 
-					this.y, 
+					this.x,
+					this.y,
 					rand(1, 3),
-					{x:rand(-10, 10), y:rand(-10, 10)},
+					{ x: rand(-100, 100), y: rand(-100, 100) },
 					this.color
 				)
 			);
@@ -82,37 +84,40 @@ class Firework extends Particle{
 	}
 }
 
-window.addEventListener("resize", function(e){
+window.addEventListener("resize", function (e) {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 });
+let lasTime = 0;
 
-let end = 0;
-
-function gameloop(){
-	let start =+ new Date();
-	ctx.fillStyle = "rgba(0, 0, 0, .5)";
+function gameloop() {
+	ctx.fillStyle = "rgba(0, 0, 0, .3)";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	
-	for(var i = 0; i < objects.length; i++){
+
+	for (var i = 0; i < objects.length; i++) {
 		var o = objects[i];
 		o.draw();
 		o.update();
-		if(o.time <= 0){
+		if (o.time <= 0) {
 			objects.splice(i, 1);
 			i--;
 		}
 	}
-	
-	if(rand(0, 10) <= 9){
-           objects.push(new Firework(rand(0, canvas.width), canvas.height, 3, {x:0, y:rand(-18, -canvas.height * .07)}));
-        }
-  
-  ctx.fillStyle = "white";
-  ctx.font = "24px Arial";
-  ctx.fillText("FPS: "+ (1000 / (start - end)).toFixed(2), 10, 40);
-	end =+ new Date();
-  window.requestAnimationFrame(gameloop);
+
+	if (rand(0, 10) <= 9) {
+		objects.push(new Firework(rand(0, canvas.width), canvas.height, 3, { x: 0, y: rand(-canvas.height * .1, -canvas.height * 0.75) }));
+	}
+
+	let now =+ new Date();
+	elapsedTime = now - lasTime;
+	lasTime = now;
+	let fps = 1000 / elapsedTime;
+	elapsedTime /= 1000;
+
+	ctx.fillStyle = "white";
+	ctx.font = "24px Arial";
+	ctx.fillText("FPS: " + fps.toFixed(2), 10, 40);
+	window.requestAnimationFrame(gameloop);
 }
 
 gameloop();
