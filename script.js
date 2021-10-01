c4dc9d37809c59cc5486f693da9ae05d9ba847d3
Alex5ander <alexsandergutierreez@gmail.com>
2021-09-30 21:37:04 -0300

    correções

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var canvas = document.getElementById("canvas");
+/** @type CanvasRenderingContext2D */
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var objects = [];
-var gravity = 0.98;
+var gravity = 9.8;
 var elapsedTime = 0;
 
 function rand(min, max) {
 	return min + Math.floor(Math.random() * ((max + 1) - min));
 }
 function randColor() {
-	return "rgb(" + rand(0, 255) + ", " + rand(0, 255) + ", " + rand(0, 255) + ")";
+	return "rgb(" + rand(0, 255) + ", " + rand(128, 255) + ", " + rand(0, 255) + ")";
 }
 
 class Particle {
@@ -21,7 +22,7 @@ class Particle {
 		this.color = color;
 		this.vel = { x: 0, y: 0 };
 		this.force = force;
-		this.time = 5;
+		this.time = 1;
 	}
 	draw() {
 		ctx.save();
@@ -42,7 +43,7 @@ class Particle {
 		this.x += this.vel.x * elapsedTime;
 		this.y += this.vel.y * elapsedTime;
 
-		this.time -= 1 * elapsedTime;
+		this.time -= elapsedTime;
 
 		this.force = { x: 0, y: 0 };
 	}
@@ -76,7 +77,7 @@ class Firework extends Particle {
 					this.x,
 					this.y,
 					rand(1, 3),
-					{ x: rand(-100, 100), y: rand(-100, 100) },
+					{ x: rand(-300, 300), y: rand(-300, 300) },
 					this.color
 				)
 			);
@@ -91,32 +92,38 @@ window.addEventListener("resize", function (e) {
 let lasTime = 0;
 
 function gameloop() {
-	ctx.fillStyle = "rgba(0, 0, 0, .3)";
+	ctx.fillStyle = "rgba(0, 0, 0, .2)";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	for (var i = 0; i < objects.length; i++) {
 		var o = objects[i];
 		o.draw();
 		o.update();
-		if (o.time <= 0) {
+		if (o.time <= 0 && o.vel.y > 0) {
 			objects.splice(i, 1);
 			i--;
 		}
 	}
 
-	if (rand(0, 10) <= 9) {
-		objects.push(new Firework(rand(0, canvas.width), canvas.height, 3, { x: 0, y: rand(-canvas.height * .1, -canvas.height * 0.75) }));
+	if (rand(0, 10) > rand(0, 10)) {
+		objects.push(new Firework(rand(0, canvas.width), canvas.height, 3, { x: 0, y: rand(-canvas.height * .1, -canvas.height * 2) }));
 	}
 
 	let now =+ new Date();
 	elapsedTime = now - lasTime;
 	lasTime = now;
 	let fps = 1000 / elapsedTime;
+
 	elapsedTime /= 1000;
 
-	ctx.fillStyle = "white";
+	let fpsText = "FPS: " + fps.toFixed(2);
+
+	ctx.fillStyle = "lightblue";
+	ctx.fillRect(0, 20, 140, 24);
+
 	ctx.font = "24px Arial";
-	ctx.fillText("FPS: " + fps.toFixed(2), 10, 40);
+	ctx.fillStyle = "white";
+	ctx.fillText("FPS: " + fps.toFixed(2), 8, 40);
 	window.requestAnimationFrame(gameloop);
 }
 
